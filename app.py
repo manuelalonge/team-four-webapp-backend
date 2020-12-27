0f35e75f408a995f0ac2d90d07768119d65c7426
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2020-12-28 00:32:53 +0700

    add form validation on app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_restful import Api, Resource, reqparse, abort
 
 #this file is the core of the app, the one who connect html pages   
@@ -11,13 +11,19 @@ video_put_args.add_argument("name", type = str, help="String required", required
 video_put_args.add_argument("views", type = int, help="String required")
 video_put_args.add_argument("likes", type = int, help="String required")
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != '#Team4':
+            error = 'Invalid credentials'
+        else:
+            return redirect(url_for('landing_page'))
+    return render_template('index.html', error=error)
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/landing")
+def landing_page():
+    return render_template("landing-page.html")
 
 @app.route("/upload")
 def upload():
