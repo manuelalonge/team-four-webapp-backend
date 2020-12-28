a7b4e409e8fae1b88f94b32121ec962988f1e80c
AleByron <alessandro.irertnuc@hotmail.it>
2020-12-28 22:48:03 +0100

    Login fixato

--- main.py
+++ main.py
@@ -1,52 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_restful import Resource, Api, reqparse
 import sqlite3 as sql
+import os
+from werkzeug.utils import secure_filename
+import requests
 
 app = Flask(__name__)
-
+app.secret_key = '12345'
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/enternew')
-def new_student():
-    return render_template('users.html')
-
-@app.route('/login')
 def login_front():
     return render_template('login.html')
 
+"""@app.route('/enternew')
+def new_student():
+    return render_template('users.html')\
 
 
-
-@app.route('/result', methods=['POST', 'GET'])
+@app.route('/signin', methods=['POST', 'GET'])
 def addrec():
     if request.method == 'POST':
         try:
-            print(1)
             email = request.form['email']
             password = request.form['password']
-            print(2)
             with sql.connect("database.db") as con:
-                print(3)
                 cur = con.cursor()
-                print(4)
                 cur.execute("INSERT INTO users (email,password)VALUES(?, ?)",(email,password) )
-                print(5)
                 con.commit()
-                #pin_col =cur.fetchall()
-                print(6)
-                msg = "evviva"
-                print(7)
 
         except:
             con.rollback()
-            msg = "error in insert operation"
+            return
 
         finally:
-            return render_template("result.html", msg=msg)
-            con.close()
+            return redirect('/upload')
+            
+#La funzione sopra serve ad aggiungere la pagina di registrazione"""
+
 
 
 @app.route('/login_back', methods=['POST','GET'])
@@ -60,28 +50,82 @@ def login_back():
             con.commit()
             control = cur.fetchall()
             if control != []:
-                msg = "login riuscito"
-                return render_template("result.html", msg=msg)
+                author = True
+                return upload_front(author = True)
             else:
-                msg = "login fallito"
-                return render_template("result.html", msg=msg)
+                return redirect('/login_failed')
 
+@app.route('/login_failed')
+def login_failed():
+    return render_template("login_failed.html")
 
+#Sopra le funzioni relative al login
 
-
-
-
-
+"""
 @app.route('/list')
 def list():
     con = sql.connect("database.db")
     con.row_factory = sql.Row
-
     cur = con.cursor()
     cur.execute("select * from users")
-
     rows = cur.fetchall()
     return render_template("list.html", rows=rows)
+
+La funzione sopra è inaccessibile agli utenti e serve a controllare il database"""
+
+
+
+UPLOAD_FOLDER = os.path.join(app.instance_path, 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_back', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        #print(request.files['files[]'])
+        # check if the post request has the file part
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect('/')
+        print(2)
+        files = request.files.getlist('files[]')
+        print(files)
+
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        print(3)
+        for file in files:
+            if file and allowed_file(file.filename):
+                print(4)
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                print(5)
+            else:
+                return redirect('/upload_failed')
+        results = "ciao sono un risultato fittizio"
+        #return redirect(url_for('success',name = user))
+        return render_template('upload.html', value=results)
+
+@app.route('/upload')
+def upload_front(author=''):
+    if author == True:
+        return render_template("upload.html")
+    else:
+        return redirect("/")
+
+@app.route('/upload_failed')
+def upload_failed():
+    return render_template("upload_failed.html")
+
+#sopra le funzioni che gestiscono l'upload dei files e mandano la risposta al frontend
+
+
+
+
+
 
 
 if __name__ == '__main__':
