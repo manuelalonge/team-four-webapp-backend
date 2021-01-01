100f0f6d122a933d94efe39629250da7f10afa54
AleByron <alessandro.irertnuc@hotmail.it>
2021-01-01 23:43:09 +0100

    tentativo_unione_front_back

---
+++ flaskr/README.md
@@ -0,0 +1,47 @@
+# Responsive Web App - Collaborative project
+
+# App Name: PhotoMatch
+
+* The project consists of a responsive web app created with a collaborative project.
+* The aim of the project is to create a simple web app accessible from any device, which allows the user after logging in and having loaded images (from the memory, or from the camera of his device in the case of mobile), to read a result, which returns the content of the uploaded images (or images)
+
+# What did we do?
+
+* With the input of our TomorrowDevs mentors we created a Responsive Web App that allows the user to load one or more photos from his / her laptop or phone
+
+# More about TomorrowDevs mentorship
+
+https://www.tomorrowdevs.com/
+
+# How did we create the web app?
+
+# What technologies did we use?
+
+# What working methodology and tools did we use?
+
+* We used a Figma's prototype to have a template to stick to. Link: https://bit.ly/3pamv5s
+* We worked through **AGILE METHODOLOGY**. Each task was created as an issue of Github and reported onto a Github Projects board linked to the repository.
+* We tracked time spent on the activities with Toggl tracker
+* We used a Jitsi room for our calls, however, in the spirit of **AGILE METHODOLOGY** we tried to keep all the communications as asynchrounous as possible
+* A Google Sheet was used to manage our daily stand-ups and keep communications asynchronous
+
+# What were the project's biggest challenges and how did we overcome them?
+
+# Meet the team
+
+## Frontend
+
+* Manuel Alonge (Team Lead)
+* Luca Bertoldi
+* Andrea Santi
+
+## Backend
+
+* Alessandro Cuntreri
+* Francesco Di Bella
+* Roberta Ruggiero 
+* Mauro Tomasoni
+
+# Deployment link
+
+https://team4-tomorrowdevs-webapp.netlify.app
---
+++ flaskr/blog.py
@@ -0,0 +1,49 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+from werkzeug.utils import secure_filename
+from flaskr.classes.image_recognized import ImageRecognized
+from flaskr.auth import login_required
+from flaskr.db import get_db
+from flaskr.__init__ import create_app
+#from flask import current_app
+import os
+
+
+bp = Blueprint('blog', __name__)
+
+app = create_app()
+UPLOAD_FOLDER = os.path.join(app.instance_path, 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/upload', methods=('GET', 'POST'))
+@login_required
+def upload_file():
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            return flash('No file part')
+        files = request.files.getlist('files[]')
+        photo_id = 0
+        results = {}
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                photo = app.config['UPLOAD_FOLDER'] + "/" + filename
+                photo_res = ImageRecognized.image_recognized(photo)
+                os.remove(app.config['UPLOAD_FOLDER'] + "/" + filename)
+                photo_id = photo_id +1
+                photo_id_st = str(photo_id)
+                results["Photo" + photo_id_st] = photo_res
+            else:
+                return flash("errore")
+        return render_template('blog/landing_page.html', value=results)
+    return render_template('blog/landing_page.html')
+    
+    
