5ed8865d06b73287ed5b798911db4fe538fc9941
Manuel Alonge <manuel.alonge@gmail.com>
2021-01-06 18:15:30 +0100

    [STARTED] submitResultsList added on index.js row 33

--- flaskr/static/js/index.js
+++ flaskr/static/js/index.js
@@ -25,9 +25,20 @@ const inputImage = document.querySelector("#images-upload");
 const modalGallery = document.querySelector(".load-images-gallery__modal");
 const imageSelected = document.querySelector(".load-images-gallery__container");
 const loadImageSquare = document.querySelector(".load-images-gallery__upload");
+const submitResultsList = document.querySelector(".load-images-gallery__submit");
 
 let newImage;
 let errorFormat;
+
+submitResultsList.addEventListener("click", function(e){
+      // e.preventDefault();
+      fetch(window.location).then(function (response) {
+        return response.json();
+        console.log(response.json())
+      }).then(function (data) {
+        console.log(data);
+      })  
+});
 
 (function init() {
     inputImage.addEventListener("change", selectImage);
@@ -297,120 +308,9 @@ function showSlides(n) {
   
   }
 
+  
 
 
-/*Take a photo function */
-(function () {
-    if (
-      !"mediaDevices" in navigator ||
-      !"getUserMedia" in navigator.mediaDevices
-    ) {
-      alert("Camera API is not available in your browser");
-      return;
-    }
-  
-    // get page elements
-    const video = document.querySelector(".video__mobile");
-    const btnPlay = document.querySelector(".btn__Play");
-    const btnPause = document.querySelector(".btn__Pause");
-    const btnScreenshot = document.querySelector(".btn__Screenshot");
-    const btnChangeCamera = document.querySelector(".btn__ChangeCamera");
-    const screenshotsContainer = document.querySelector(".container__screenshots");
-    const canvas = document.querySelector(".container__canvas");
-    //const devicesSelect = document.querySelector(".devicesSelect");
-    var removeClass = document.querySelector(".btn__circle");
 
 
-    //btn visible 
-    
-    removeClass.addEventListener("click", function() {
-
-      var element = document.getElementByClassName("section");
-      element.classList.remove("section-visible");
-    } )         
- 
-
-
-    // video constraints
-    const constraints = {
-      video: {
-        width: {
-          min: 1280, 
-          ideal: 1920, 
-          max: 2560,
-        },
-        height: {
-          min: 720, 
-          ideal: 1080, 
-          max: 1440,
-        },
-      },
-    };
-  
-    // use front face camera
-    let useFrontCamera = true;
-  
-    // current video stream
-    let videoStream;
-  
-    // handle events
-    // play
-    btnPlay.addEventListener("click", function () {
-      video.play();
-      btnPlay.classList.add("is-hidden");
-      btnPause.classList.remove("is-hidden");
-    });
-  
-    // pause
-    btnPause.addEventListener("click", function () {
-      video.pause();
-      btnPause.classList.add("is-hidden");
-      btnPlay.classList.remove("is-hidden");
-    });
-  
-    // take screenshot
-    btnScreenshot.addEventListener("click", function () {
-      const img = document.createElement("img");
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      canvas.getContext("2d").drawImage(video, 0, 0);
-      img.src = canvas.toDataURL("image/png");
-      screenshotsContainer.prepend(img);
-    });
-  
-    // switch camera
-    btnChangeCamera.addEventListener("click", function () {
-      useFrontCamera = !useFrontCamera;
-  
-      initializeCamera();
-    });
-  
-    // stop video stream
-    function stopVideoStream() {
-      if (videoStream) {
-        videoStream.getTracks().forEach((track) => {
-          track.stop();
-        });
-      }
-    }
-  
-    // initialize
-    async function initializeCamera() {
-      stopVideoStream();
-      constraints.video.facingMode = useFrontCamera ? "user" : "environment";
-  
-      try {
-        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = videoStream;
-      } catch (err) {
-        alert("Could not access the camera");
-      }
-    }
-  
-    initializeCamera();
-  })();
-
-
-/* Images Upload Functions (From Desktop) 
-*/
 
